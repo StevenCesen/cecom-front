4031fb91d59a4245eedc9f9e134af746a717490b
StevenCesen <steven.cesen@unl.edu.ec>
2025-02-20 15:33:48 -0500

    Cambio de nombre APP

--- app/hooks/env.js
+++ app/hooks/env.js
@@ -1,5 +1,6 @@
 export const URL_BASE='https://cecom.ec/apiv1/public/api/';
-export const COMMERCIAL_TYPE='HANNAH EC';
+export const URL_BASEs='http://localhost:8000/api/';
+export const COMMERCIAL_TYPE='CECOM EC';
 /**
  * AMBIENTE DE FACTURACIÓN
  * 1. - PRUEBAS: 1
@@ -10,3 +11,6 @@ export const CONTABILIDAD="NO";
 export const URL_ROOT="https://cecom.ec/apiv1/";
 export const URL_RECEP_PROD='https://cecom.ec/apiv1/services/sendSRI.php';
 export const URL_AUTH_PROD='https://cecom.ec/apiv1/services/authSRI.php';
+export const URL_ROOTs="http://localhost/apianyplace/";
+export const URL_RECEP_PRODs='http://localhost/apianyplace/services/sendSRI.php';
+export const URL_AUTH_PRODs='http://localhost/apianyplace/services/authSRI.php';
